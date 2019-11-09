e1a15bff1ec52b7a45b887dca0af9fc6bc9bd90d
abmaelsilva02 <abmaelsilva02@gmail.com>
2019-11-09 20:16:57 -0300

    merge

--- PR/PR.Infra/Repositories/ReportRepository.cs
+++ PR/PR.Infra/Repositories/ReportRepository.cs
@@ -1,8 +1,10 @@
-﻿using PR.Domain.Entities;
+﻿using Dapper;
+using PR.Domain.Entities;
 using PR.Domain.Repositories;
 using PR.Infra.Infra;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,52 +21,72 @@ namespace PR.Infra.Repositories
 
         public async Task<Report> GetById(Guid id)
         {
-            var sql = "";
+            var sql = "SELECT * FROM Report WHERE Id = @id";
             using (var db = _DB.GetCon())
             {
-
+                var report = await db.QueryAsync<Report>(sql, new { id });
+                return report.FirstOrDefault();
             }
-            throw new NotImplementedException();
         }
 
         public void Insert(Report relatorio)
         {
-            var sql = "";
+            var sql = "INSERT INTO Report " +
+                "(Id,ConstructionId,ResponsibleId,Title,Description,Image,DateReport) " +
+                "VALUES" +
+                "(@Id,@ConstructionId,@ResponsibleId,@Title,@Description,@Image,@DateReport)";
             using (var db = _DB.GetCon())
             {
-
+                db.Execute(sql, new
+                {
+                    ResponsibleId = relatorio.Responsible.Id,
+                    ConstructionId = relatorio.Construction.Id,
+                    relatorio.Title,
+                    relatorio.Description,
+                    relatorio.Image,
+                    relatorio.Id,
+                    relatorio.DateReport
+                });
             }
-            throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Report>> ListByConstructionId(Guid constructionId)
         {
-            var sql = "";
+            var sql = "SELECT * FROM Report WHERE ConstructionId = @constructionId";
             using (var db = _DB.GetCon())
             {
-
+                var report = await db.QueryAsync<Report>(sql, new { constructionId });
+                return report;
             }
-            throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Report>> ListByResponsibleId(Guid responsabileId)
         {
-            var sql = "";
+            var sql = "SELECT * FROM Report WHERE Responsible = @responsabileId";
             using (var db = _DB.GetCon())
             {
-
+                var report = await db.QueryAsync<Report>(sql, new { responsabileId });
+                return report;
             }
-            throw new NotImplementedException();
         }
 
         public void Update(Report relatorio)
         {
-            var sql = "";
+            var sql = "UPDATE Report SET" +
+                "Title = @Title" +
+                ",Image = @Image" +
+                ",Description = @Description" +
+                " WHERE Id = @Id";
             using (var db = _DB.GetCon())
             {
-
+                db.Execute(sql, new
+                {
+                    relatorio.Title,
+                    relatorio.Description,
+                    relatorio.Image,
+                    relatorio.Id
+                });
             }
-            throw new NotImplementedException();
         }
     }
 }
